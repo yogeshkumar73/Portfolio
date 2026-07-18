47e4646c2b02641d0bd8593563119da2bb337e5d
sonuprajapatee <your-email@gmail.com>
2026-07-19 00:22:24 +0530

    first commit

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./projects.css";
+import "./Projects.css";
 
 function Projects() {
 
